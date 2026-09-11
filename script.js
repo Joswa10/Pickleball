3fe01d25edb287802856bb440ee823034e9527c3
Joshua Aromin <icey.dogo@gmail.com>
2026-09-12 02:18:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -132,14 +132,14 @@ function saveMatch() {
   const s2 = parseInt(document.getElementById('score2').textContent, 10) || 0;
   const s3 = parseInt(document.getElementById('score3').textContent, 10) || 0;
 
-  const team1Score = s0 + s1;
-  const team2Score = s2 + s3;
+  const team1Total = s0 + s1;
+  const team2Total = s2 + s3;
 
   const matchRecord = {
-    team1: [p0, p1],
-    team2: [p2, p3],
-    team1Score,
-    team2Score,
+    team1: [{ name: p0, score: s0 }, { name: p1, score: s1 }],
+    team2: [{ name: p2, score: s2 }, { name: p3, score: s3 }],
+    team1Total,
+    team2Total,
     time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
   };
 
@@ -185,16 +185,17 @@ function renderHistory() {
     const item = document.createElement('div');
     item.className = 'history-item';
     
-    const team1Text = match.team1.join(' & ');
-    const team2Text = match.team2.join(' & ');
+    // Individual players with scores
+    const team1Str = `${match.team1[0].name} (${match.team1[0].score}) & ${match.team1[1].name} (${match.team1[1].score})`;
+    const team2Str = `${match.team2[0].name} (${match.team2[0].score}) & ${match.team2[1].name} (${match.team2[1].score})`;
     
     let outcomeHTML = '';
-    if (match.team1Score > match.team2Score) {
-      outcomeHTML = `🏆 <b>${team1Text}</b> (${match.team1Score}) vs ${team2Text} (${match.team2Score})`;
-    } else if (match.team2Score > match.team1Score) {
-      outcomeHTML = `${team1Text} (${match.team1Score}) vs 🏆 <b>${team2Text}</b> (${match.team2Score})`;
+    if (match.team1Total > match.team2Total) {
+      outcomeHTML = `🏆 <b style="color: #2e7d32;">${team1Str}</b> [${match.team1Total}] vs ${team2Str} [${match.team2Total}]`;
+    } else if (match.team2Total > match.team1Total) {
+      outcomeHTML = `${team1Str} [${match.team1Total}] vs 🏆 <b style="color: #2e7d32;">${team2Str}</b> [${match.team2Total}]`;
     } else {
-      outcomeHTML = `🤝 <b>TIE:</b> ${team1Text} (${match.team1Score}) vs ${team2Text} (${match.team2Score})`;
+      outcomeHTML = `🤝 <b>TIE:</b> ${team1Str} [${match.team1Total}] vs ${team2Str} [${match.team2Total}]`;
     }
     
     item.innerHTML = `
